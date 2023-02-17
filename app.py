76c17b88eaf7c4ff0e42e1818e6ba9a4efcee253
Mayur Patel <pmayur0680@gmail.com>
2023-02-17 12:49:12 -0600

    Todo Flask-Jinja-SQLAlchemy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -17,9 +17,44 @@ class Todo(db.Model):
         return '<Task %r>' %self.id
 
 # create index route
-@app.route('/')
+@app.route('/', methods=['POST', 'GET']) # GET default
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all() # or first() for top
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')    
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an issue deleting task'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content'] # setting current task (line 48) content to one form input
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
